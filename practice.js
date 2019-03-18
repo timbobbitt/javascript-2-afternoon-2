026090feed88f1a71c4c87ad386528eda7bb6311
TIMOTHY BOBBITT <timothybobbitt@TIMOTHYs-MacBook-Pro-2.local>
2019-03-17 23:17:14 -0500

    message

--- practice.js
+++ practice.js
@@ -18,8 +18,10 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function first(arr){
 
-
+}
+arr[0];
 
 ////////// PROBLEM 2 //////////
 
@@ -33,7 +35,7 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+arr[2];
 
 
 ////////// PROBLEM 3 //////////
@@ -48,6 +50,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper(family){
+  for (i = 0; i < family.length; i++);
+alert(family[i]);
+}
+
 
 
 
@@ -63,7 +70,8 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for (i = 0; i < letters.length; i++)
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +86,13 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(nums){
+  for (i = 0; i < nums.length; i++){
+    if (i % 2 === 0)
+    return(nums[i]);
+  } 
 
+  }
 
 
 
